5dec856a10f91ea793f2095f63ce48985e5b1fb9
roshnirajan1106 <roshnirajan8888@gmail.com>
2025-07-27 00:28:27 +0530

    initial commit

---
+++ src/main/java/com/url_shortner/demo/BuildingUrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.url_shortner.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingUrlShortnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingUrlShortnerApplication.class, args);
+	}
+
+}
